861a8c861a92bb5ca506e33548a89f805e839fde
Akenokoru <akenokoru.pan@gmail.com>
2021-07-21 23:59:31 +0800

    init

---
+++ scripts/build.js
@@ -0,0 +1,30 @@
+// monorepo complier
+
+// node
+const fs = require('fs')
+const execa = require('execa')
+
+const dirs = fs.readdirSync('packages').filter(path => {
+  if(!fs.statSync(`packages/${path}`).isDirectory()) {
+    return false;
+  }
+  return true;
+})
+console.log(dirs);
+
+async function build(target) {
+  console.log('target--', target)
+  await execa('rollup', ['-c', '--environment', `TARGET: ${target}`])
+}
+
+async function runParallel(dirs, iterFn) {
+  let result = []
+  for(let item of dirs) {
+    result.push(iterFn(item))
+  }
+  return Promise.all(result)
+}
+
+runParallel(dirs, build).then(() => {
+  console.log('success')
+})
